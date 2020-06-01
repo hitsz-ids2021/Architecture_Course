c91b3f191ceab04a9c68c5ee5694381e9e85a5fc
caozs <zhsh.cao@gmail.com>
2020-06-01 14:47:22 +0800

    update foo3.c

--- Linking/duplicate_symbol_names_example/foo3.c
+++ Linking/duplicate_symbol_names_example/foo3.c
@@ -4,16 +4,20 @@
 void show_pointer(void *x);
 void f(void);
 
+
+int y; 
 int x; 
 
 int main()  
 {
     x = 1;
-    printf("Previously, x = 0x%x (&x+1) = 0x%x \n", x, *(&x+1));
-    printf("The address of x and y are %p and &x+1 = %p\n", &x, &x+1);
+    y = -1;
+    printf("Previously, x = 0x%x y = 0x%x \n", x, y);
+    printf("The address of x and y are %p, %p, and &x+1 = %p\n", &x, &y, &x+1);
     show_pointer((void *)&x);
     f(); 
-    printf("Now, x = 0x%x  *(&x+1) = 0x%x\n", x, *(&x+1)); 
+    printf("Now, x = 0x%x y = 0x%x  *(&x+1) = 0x%x\n", 
+	   x, y, *(&x+1)); 
     return 0;
 } 
 /* $end foo3 */
